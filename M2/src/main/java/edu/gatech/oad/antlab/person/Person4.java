61680734ac043fc4b1a94d38f30eec55cf4f181e
manoajk <manoajk@yahoo.com>
2017-01-27 14:32:14 -0500

    Updated Person4 (Manoaj's file) to reflect javadoc

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      char[] charArray = input.toCharArray();
+      for (int i = 0; i < old.length; i++) {
+        charArray[i]++;
+      }
+      return new String(charArray);
     }
     
     /**
