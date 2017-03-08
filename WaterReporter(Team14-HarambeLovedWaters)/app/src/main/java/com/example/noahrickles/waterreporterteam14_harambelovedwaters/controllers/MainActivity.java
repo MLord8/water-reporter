40b19f652eccb9dd2092850a835a24845e24f804
nrickles3 <nrickles3@gatech.edu>
2017-03-07 22:56:38 -0500

    Removed email icon button from main activity

--- WaterReporter(Team14-HarambeLovedWaters)/app/src/main/java/com/example/noahrickles/waterreporterteam14_harambelovedwaters/controllers/MainActivity.java
+++ WaterReporter(Team14-HarambeLovedWaters)/app/src/main/java/com/example/noahrickles/waterreporterteam14_harambelovedwaters/controllers/MainActivity.java
@@ -59,14 +59,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 reportStrings);
         reports.setAdapter(listAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
