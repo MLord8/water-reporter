1523d233966324f24f3a241f28bfcd34222fa47b
manoajk <manoajk@yahoo.com>
2017-04-22 16:35:36 -0400

    Put Firebase JS code in scripts.js

---
+++ WaterReporterWebsite/scripts.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+apiKey: "AIzaSyAd45R67ts78jB2xXMKJ4n2YoD2xXU2y7M",
+authDomain: "https://harambe-loved-waters-reporter.firebaseapp.com",
+databaseURL: "https://harambe-loved-waters-reporter.firebaseio.com",
+// storageBucket: "<BUCKET>.appspot.com",
+// messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
